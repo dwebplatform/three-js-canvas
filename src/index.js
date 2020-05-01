41fb0f8d4d4ce1ffe1ed5ddce5f0151ab22797f9
Vladimir <karpov-vb-1996@mail.ru>
2020-05-01 11:50:05 +0300

    sphere rotaion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
+const  TWEEN = require('@tweenjs/tween.js');
 
 const globalScene ={
     scene:null
@@ -31,56 +32,147 @@ const sketch = ({ context }) => {
   renderer.setClearColor("hsl(10, 60%, 70%)", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
+  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 500);
   camera.position.set(2, 2, -4);
   camera.lookAt(new THREE.Vector3());
 
   // Setup mouse orbit controller
   const controls = new THREE.OrbitControls(camera);
-
+  const coords = { x: 0, y: 0 };
+  
   // Setup your scene
   const scene = new THREE.Scene();
   globalScene.scene = scene;
   // Create a bunch of 'particles' in a group for the BG
-  let allCubes = [];
+  
   const material =new THREE.MeshBasicMaterial({color: "green"}) ;
 
-    let initialX = 0;
-    let initialY = 0;
-    let initialZ= 0;
+    
+        
+      
+
+  
+  const perfectPlace =[{
+    x:0,
+    y:0,
+    z:0,
+  },{
+    x:0,
+    y:10,
+    z:0,
+  },{
+    x:0,
+    y:20,
+    z:0,
+  },{
+    x:0,
+    y:30,
+    z:0,
+  },
+    {
+      x:0,
+      y:40,
+      z:0,  
+    
+  },{
+    x:0,
+    y:50,
+    z:0,
+  
+},{
+  x:0,
+  y:60,
+  z:0,
+
+},{
+  x:0,
+  y:70,
+  z:0,
+
+},
+{ x:0,
+  y:80,
+  z:0,
+
+  
+},
+{ x:0,
+  y:90,
+   z:0,
+
+  
+}
+];  
+
+  const mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(1,2,2),
+    material
+    );
+
+    const Boxes = [];
     for(let i=0;i<30;i++){
+      const mesh = new THREE.Mesh(
+        new THREE.SphereGeometry(Math.random()*5,32,32),
+        material
+        );
+        let tetta = Math.PI* Math.random();
+        let phy = 2*Math.PI* Math.random();
+        let x = 30* Math.sin(tetta)*Math.cos(phy);
+        let y = 30* Math.sin(tetta)* Math.sin(phy);
+        let z =  30* Math.cos(tetta);
+        let randomPosition = [Math.floor(x ),y,Math.floor(z) ];
+        mesh.position.set(...randomPosition);
+        Boxes.push(mesh);
+        scene.add(mesh);
+      }
+
+    
+      
+
+    const particles = [];
+    particles.push({
+      el: mesh,
+      Velosity3d:[10,5,3],
+
+    });
+
+    perfectPlace.forEach((place,i)=>{
+
+    //    const tween = new TWEEN.Tween(Boxes[i].position)
+    //   .to(place,1000)
+    //  .onUpdate(function() { 
+    //       const {_object:object} = this;
+    //   const {x,y} = object;
+    //   Boxes[i].position.x = x;
+    //   Boxes[i].position.y = y;
+    
+    //         }).start();
+      
+    });
+  //     const mesh = new THREE.Mesh(
+    //       new THREE.BoxGeometry(1,1,1),
+    //       material);
+    //       mesh.name="box-"+i;
+    //       mesh.position.set(x*2,y,z*2);
+    //       scene.add(mesh);    
+
+    //         allCubes.push({
+    //             mesh: mesh,
+                 
+    //         });
+    //         if(initialZ<4){
+    //           initialZ++;
+    //         }
+    //         else{
+    //           initialX++;
+    //             initialZ=0;
+    //         }
         
-        
-        let x = initialX;
-        let y = initialY;
-        let z = initialZ;
-        
-            
-            allCubes.push({
-                
-                position:[x*2,y*2,z]
-            });
-            if(initialX<4){
-                    initialX++;
-            }
-            else{
-                initialY++;
-                initialX=0;
-            }
-        
-    }
+    // }
 
 
   // Add a little mesh to the centre of the screen
-allCubes.forEach(({position},index)=>{
-    const [x,y,z] = position;
-    const mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1,1,1),
-        material);
-        mesh.name="box-"+index;
-        mesh.position.set(x,y,z);
-        scene.add(mesh);
-});
+ 
 
   // draw each frame
     return {
@@ -93,27 +185,31 @@ allCubes.forEach(({position},index)=>{
     },
     // And render events here
     render({ time, deltaTime }) {
-      // Rotate background subtly
 
-        
+ 
 
+      
+  
+        //  if( el.position.x>10 || el.position.x<-10){
+        //   Velosity3d[0]= -Velosity3d[0];
+        // }
+        // if(el.position.y>10 || el.position.y<-10){
+        //   Velosity3d[1]= -Velosity3d[1];
+          
+        // }
 
-        if(dissapear){
-            allCubes.splice(1,1);
-        
-            let randomElem = Math.floor( Math.random()*scene.children.length);
+        // if( el.position.z>10 || el.position.z<-10){
+        //   Velosity3d[2]= -Velosity3d[2];
+        // }
+          // mesh.position.x+= Velosity3d[0]*deltaTime;
+           
+   
+  
 
-                scene.remove(scene.children[randomElem]);
-            // scene.children.length = scene.children.length -1;
-
-        dissapear = false;
-    }
-
-    
-
-        
-    // controls.update();
-    renderer.render(scene, camera);
+      
+      // controls.update();
+      TWEEN.update();
+      renderer.render(scene, camera);
     },
     // Dispose of WebGL context (optional)
     unload() {
@@ -124,8 +220,5 @@ allCubes.forEach(({position},index)=>{
 
 };
 
-setInterval(()=>{
-
-    dissapear = true;
-},1000);
+ 
 canvasSketch(sketch, settings);
